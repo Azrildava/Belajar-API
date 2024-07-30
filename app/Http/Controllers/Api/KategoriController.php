91f3ad02cfca4c92fdc4f4b44dbf0ad47bd23ccb
Azrildava <azrililham937@gmail.com>
2024-07-30 15:00:11 +0700

    Api

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -71,4 +71,54 @@ class KategoriController extends Controller
             ], 404);
         }
     }
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(),[
+            'nama_kategori' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            $kategori = Kategori::findOrFail($id);
+            $kategori->nama_kategori = $request->nama_kategori;
+            $kategori->slug = Str::slug($request->nama_kategori);
+            $kategori->save();
+            return response()->json([
+                'success' => true,
+                'message' => 'data berhasil diperbarui',
+                'data' => $kategori,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+            'success' => false,
+            'message' => 'terjadi kesalahan',
+            'data' => $e->getMessage(),
+            ], 500);
+        }
+    }
+    public function destroy($id)
+    {
+       try {
+        $kategori = Kategori::findOrFail($id);
+        $kategori->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'kategori' .$kategori->nama_kategori . 'berhasil dihapus',
+            'data' => $kategori,
+        ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+            'success' => false,
+            'message' => 'data tidak ditemukan',
+            'data' => $e->getMessage(),
+            ], 404);
+        }
+    }
 }
---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Str;
+use Validator;
+
+class UserController extends Controller
+{
+     public function index()
+    {
+        $user = User::latest()->get();
+        $res = [
+            'success' => true,
+            'message' => 'Daftar user',
+            'data' => $user,
+        ];
+        return response()->json($res, 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return response()->json([
+                'success' => true,
+                'message' => 'data berhasil dibuat',
+                'data' => $user,
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'terjadi kesalahan',
+                'data' => $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            return response()->json([
+                'success' => true,
+                'message' => 'detail user',
+                'data' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'data tidak ditemukan',
+                'data' => $e->getMessage(),
+            ], 404);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validasi gagal',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        try {
+            $user = User::findOrFail($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+            return response()->json([
+                'success' => true,
+                'message' => 'data berhasil diperbarui',
+                'data' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'terjadi kesalahan',
+                'data' => $e->getMessage(),
+            ], 500);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'user' . $user->nama_user . 'berhasil dihapus',
+                'data' => $user,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'data tidak ditemukan',
+                'data' => $e->getMessage(),
+            ], 404);
+        }
+    }
+}
